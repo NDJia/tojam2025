85c0d7ecfa6d85fb9904a0e755da4c4e1f7e0de4
NDJia <82289798+NDJia@users.noreply.github.com>
2025-05-11 17:45:41 -0400

    you win screen

--- Scripts/pausemenu.gd
+++ Scripts/pausemenu.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 
-#func _ready():
-	#$Resume.pressed.connect(resume)
-	#$ToMainMenu.pressed.connect(to_menu())
+func _ready():
+	$Resume.pressed.connect(resume)
+	$ToMainMenu.pressed.connect(to_menu)
 	
 func resume():
-	pass
+	hide()
+	get_tree().paused = false
 
 func to_menu():
-	pass
+	get_tree().change_scene_to_file("res://Scenes/UI/mainmenu.tscn")
